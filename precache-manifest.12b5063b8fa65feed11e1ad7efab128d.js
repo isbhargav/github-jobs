6ba195d20bede24d5988ab75683d15a7f5e128e8
isbhargav <ladbhargav@yahoo.co.in>
2020-10-30 15:19:27 -0400

    Updates

---
+++ precache-manifest.12b5063b8fa65feed11e1ad7efab128d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8539a617c2e92916f84180393c5959f",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "38569e6b73e88313c3f9",
+    "url": "/github-jobs/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "c81cf11c5af76860cf97",
+    "url": "/github-jobs/static/js/2.2dc99723.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs/static/js/2.2dc99723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38569e6b73e88313c3f9",
+    "url": "/github-jobs/static/js/main.948e96d1.chunk.js"
+  },
+  {
+    "revision": "818ca7b73b62caac58fd",
+    "url": "/github-jobs/static/js/runtime-main.90d76a25.js"
+  }
+]);
